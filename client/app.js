3790932ed5d46f1a2b1edca9cc15c3c936558cc8
Shubham Kumar <shubhamkumar007@gmail.com>
2018-11-14 22:14:05 -0500

    minor changes, fixes

--- client/app.js
+++ client/app.js
@@ -8,12 +8,13 @@ function ViewModel(){
 	var self = this;
 	self.goals = ko.observableArray();
 	self.goalInputName = ko.observableArray();
+	self.goalInputType = ko.observable();
 	self.goalInputDeadline = ko.observableArray();
 
 	self.addGoal = function(){
-		var name = $(#name).val();
-		var type = $(#type).val();
-		var deadline = $(#deadline).val();
+		var name = $('#name').val();
+		var type = $('#type').val();
+		var deadline = $('#deadline').val();
 
 		self.goals.push({
 			name: name,
@@ -21,13 +22,22 @@ function ViewModel(){
 			deadline: deadline
 		});
 
+        //save data t server
 		$.ajax({
 			url: "http://localhost:3000/goals",
 			data: JSON.stringify({
                   "name": name,
                   "type": type,
                   "deadline": deadline  
-			});
+			}),
+			type: "POST",
+			contentType: "application/json",
+			success: function(data){
+				console.log('Goal added...');
+			}
+			error: function(xhr,status,err){
+				console.log(err);
+			}
 		});
 	}
 
